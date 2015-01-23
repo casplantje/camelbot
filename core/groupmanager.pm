1e0221ddf6e301d08e7a03517587cc3fed58e461
casplantje <casplantje@casplantje.nl>
2015-01-23 01:08:13 +0100

    Added an initial query function to test the database acts as expected.

--- core/groupmanager.pm
+++ core/groupmanager.pm
@@ -6,7 +6,27 @@ use DBI;
 # Will contain the code to manage groups
 #   there will also be functions to allow plugins to inject user/group properties (for example to use the twitch api)
 
-my $dbh = DBI->connect("dbi:SQLite:dbname=groupmanagement.db","","");
+my $database = DBI->connect("dbi:SQLite:dbname=groupmanagement.db","","")
+	or die $DBI::errstr;
 
+sub getUserGroups
+{
+	my ($username) = @_;
+	my $stmt = qq(SELECT DISTINCT groups.name from users LEFT JOIN usergroups LEFT JOIN groups where users.name="$username");
+	my $sth = $database->prepare( $stmt);
+	my $rv = $sth->execute() or die $DBI::errstr;
+	my @result;
+	
+	while(my @row = $sth->fetchrow_array()) {
+		push @result, $row[0];
+	}
+	return @result;
+}
+	my @groups = getUserGroups("casplantje");
+	print "Casplantje's groups:\n";
+	foreach my $group (@groups)
+	{
+			print "$group\n";
+	}
 
 1;
