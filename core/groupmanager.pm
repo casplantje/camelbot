bbfa3cafc2aa3e4be3d45894fbc9e46399d1a871
casplantje <casplantje@casplantje.nl>
2015-02-17 01:24:14 +0100

    Changes to group management * Fixed getUserGroups query(joins were wrong) * added getUserGroupPrivileges, getUserUserPrivileges and getUserPrivileges * added delete triggers to sqlite database * added notes for database development * added a file with a list of all non-standard perl module uses

--- core/groupmanager.pm
+++ core/groupmanager.pm
@@ -1,5 +1,6 @@
 package core::groupmanager;
 
+use List::MoreUtils qw(zip);
 use strict;
 use DBI;
 
@@ -12,7 +13,10 @@ my $database = DBI->connect("dbi:SQLite:dbname=groupmanagement.db","","")
 sub getUserGroups
 {
 	my ($username) = @_;
-	my $stmt = qq(SELECT DISTINCT groups.name from users LEFT JOIN usergroups LEFT JOIN groups where users.name="$username");
+	my $stmt = qq(SELECT DISTINCT groups.name FROM users 
+					INNER JOIN usergroups ON users.id = usergroups.userid 
+					INNER JOIN groups ON groups.id = usergroups.groupid 
+					WHERE users.name="$username");
 	my $sth = $database->prepare( $stmt);
 	my $rv = $sth->execute() or die $DBI::errstr;
 	my @result;
@@ -22,8 +26,55 @@ sub getUserGroups
 	}
 	return @result;
 }
-	my @groups = getUserGroups("casplantje");
-	print "Casplantje's groups:\n";
+
+sub getUserUserPrivileges
+{
+	my ($username) = @_;
+	my $stmt = qq(SELECT DISTINCT privileges.name FROM users
+					INNER JOIN userprivileges ON userprivileges.userid = users.id 
+					INNER JOIN privileges ON userprivileges.privilegeid = privileges.id
+					WHERE users.name="$username");
+	my $sth = $database->prepare( $stmt);
+	my $rv = $sth->execute() or die $DBI::errstr;
+	my @result;
+	
+	while(my @row = $sth->fetchrow_array()) {
+		push @result, $row[0];
+	}
+	return @result;
+}
+
+sub getUserGroupPrivileges
+{
+	my ($username) = @_;
+	my $stmt = qq(SELECT DISTINCT privileges.name FROM users 
+					INNER JOIN usergroups ON users.id = usergroups.userid 
+					INNER JOIN groups ON groups.id = usergroups.groupid 
+					INNER JOIN groupprivileges ON groups.id = groupprivileges.groupid 
+					INNER JOIN privileges ON privileges.id = groupprivileges.privilegeid 
+					WHERE users.name="$username");
+	my $sth = $database->prepare( $stmt);
+	my $rv = $sth->execute() or die $DBI::errstr;
+	my @result;
+	
+	while(my @row = $sth->fetchrow_array()) {
+		push @result, $row[0];
+	}
+	return @result;
+}
+
+sub getUserPrivileges
+{
+	my ($username) = @_;
+	my @userPrivileges = getUserUserPrivileges($username);
+	my @groupPrivileges = getUserGroupPrivileges($username);
+	return zip(@userPrivileges, @groupPrivileges);
+}
+
+
+# test code
+	my @groups = getUserPrivileges("casplantje");
+	print "Casplantje's privileges:\n";
 	foreach my $group (@groups)
 	{
 			print "$group\n";
